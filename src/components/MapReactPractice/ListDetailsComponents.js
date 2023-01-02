5789b081ac122f697a5dd8896256c8d769382c8c
pawannagar <pawannagar1996@gmail.com>
2023-01-02 19:51:16 +0530

    map

--- src/components/MapReactPractice/ListDetailsComponents.js
+++ src/components/MapReactPractice/ListDetailsComponents.js
@@ -5,15 +5,15 @@ import ListLayout from "./ListLayout";
 const staticList = [
   {
     id: "01",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fokcredit.in%2Fblog%2Fhow-to-start-tour-and-travel-business%2F&psig=AOvVaw25UX9ThN5nFAXO9o5OJA9p&ust=1672746578432000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjthd_oqPwCFQAAAAAdAAAAABAE",
+    imgURL:"img.png",
     altName:"alt img One",
-    heading: "Heading One",
+    heading: "Heading 1",
     discription:
       "Lorem Ipsum doesn exist because people think the content is meaningless window dressing, only there to be decorated by designers who can be bothered to read. Lorem Ipsum exists because words are powerful. If you fill up your page with draft copy about your client’s business, they will read it. They will comment on it.",
   },
   {
     id: "02",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fokcredit.in%2Fblog%2Fhow-to-start-tour-and-travel-business%2F&psig=AOvVaw25UX9ThN5nFAXO9o5OJA9p&ust=1672746578432000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjthd_oqPwCFQAAAAAdAAAAABAE",
+    imgURL:"https://cdn.pixabay.com/photo/2022/01/03/01/00/ruins-6911495_960_720.jpg",
     altName:"alt img two",
     heading: "Heading two",
     discription:
@@ -21,7 +21,7 @@ const staticList = [
   },
   {
     id: "03",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fokcredit.in%2Fblog%2Fhow-to-start-tour-and-travel-business%2F&psig=AOvVaw25UX9ThN5nFAXO9o5OJA9p&ust=1672746578432000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjthd_oqPwCFQAAAAAdAAAAABAE",
+    imgURL:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fkoinoniatravels.com%2F&psig=AOvVaw1FnujmhOnB_ZMg7wzHBoFX&ust=1672754754825000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJiShZqHqfwCFQAAAAAdAAAAABAE",
     altName:"alt img Three",
     heading: "Heading Three",
     discription:
@@ -29,7 +29,7 @@ const staticList = [
   },
   {
     id: "04",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fokcredit.in%2Fblog%2Fhow-to-start-tour-and-travel-business%2F&psig=AOvVaw25UX9ThN5nFAXO9o5OJA9p&ust=1672746578432000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjthd_oqPwCFQAAAAAdAAAAABAE",
+    imgURL:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fokcredit.in%2Fblog%2Fhow-to-start-tour-and-travel-business%2F&psig=AOvVaw25UX9ThN5nFAXO9o5OJA9p&ust=1672746578432000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJjthd_oqPwCFQAAAAAdAAAAABAE",
     altName:"alt img Four",
     heading: "Heading Four",
     discription:
@@ -40,11 +40,11 @@ const staticList = [
 function ListDetails(){
 
     function renderList(data){
-        const {id, img, altName, heading, discription}=data
+        const {id, imgURL, altName, heading, discription}=data
         return(
             <ListLayout
             key={id}
-            img={img}
+            imgURL={imgURL}
             altName={altName}
             heading={heading}
             discription={discription}
--- src/components/MapReactPractice/ListLayout.js
+++ src/components/MapReactPractice/ListLayout.js
@@ -1,14 +1,16 @@
-import "../../App.css"
+import "../../App.css";
 
-
-export default function ListLayout(props){
-const {img, heading, discription,altName}=props
-    return(
-        <li className="list-design">
-            <div className="list-design-img"><img src={img} alt={altName} /></div>
-            <div className="list-design-text"><h2>{heading}</h2>
-            <p>{discription}</p></div>
-            
-        </li>
-    )
+export default function ListLayout(props) {
+  const { imgURL, heading, discription, altName } = props;
+  return (
+    <li className="list-design">
+      <div className="list-design-img">
+        <img src={imgURL} alt={altName} />
+      </div>
+      <div className="list-design-text">
+        <h2>{heading}</h2>
+        <p>{discription}</p>
+      </div>
+    </li>
+  );
 }
