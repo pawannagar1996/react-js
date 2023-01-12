28383d4128e0c481dbdbf4aeded1827b003d08ec
pawannagar <pawannagar1996@gmail.com>
2023-01-12 21:54:22 +0530

    change status

--- src/components/todoList2/todolistUncontrll.js
+++ src/components/todoList2/todolistUncontrll.js
@@ -1,55 +1,22 @@
 import { useRef, useState } from "react";
 import "./todolistUncontroll.css";
 
-// export default function TodoListUncntl() {
-//   const inputref = useRef();
-//   const [todos, updateTodos] = useState([]);
-
-//   function handleAddTodo(event) {
-//     console.log(inputref.current.value);
-
-//     const newText = inputref.current.value;
-//     const todo = { id: Date.now(), text: newText };
-//     const newTodos = JSON.parse(JSON.stringify(todos));
-//     newTodos.push(todo);
-//     updateTodos(newTodos);
-//     inputref.current.value = "";
-//   }
-
-//   function renderList(todoItem) {
-//     const { id, text } = todoItem;
-//     return (
-//       <li key={id}>
-// <span>{id}</span>
-//         <span>{text}</span>
-//         {/* <span>{status}</span> */}
-//       </li>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       <div>
-//         <input type="text" ref={inputref} />
-//         <input type="button" value="Add" onClick={handleAddTodo} />
-//       </div>
-//       <ul>{todos.map(renderList)}</ul>
-//     </div>
-//   );
-// }
-
 export default function TodoListUncntl() {
   const currentRef = useRef();
   const [currentValue, updateValue] = useState([]);
   const [oldSerialNumber, updateSerialNumber] = useState(1);
+  const [oldStatus,newStatus] = useState("pending")
 
-  function handleAddTodo(event) {
+
+  function handleAddTodo() {
     const newText = currentRef.current.value;
-    const todos = {
+    let todos ={
       id: Date.now(),
       text: newText,
       serialNumber: oldSerialNumber,
+      status:oldStatus
     };
+  ;
     const newTodos = JSON.parse(JSON.stringify(currentValue));
     newTodos.push(todos);
     updateValue(newTodos);
@@ -57,16 +24,24 @@ export default function TodoListUncntl() {
     updateSerialNumber(oldSerialNumber + 1);
   }
 
+  function changeLi(event){
+    newStatus(event.target.value="done");
+
+  }
+
+
   function renderList(items) {
-    const { id, text, serialNumber } = items;
+    const { id, text, serialNumber, status} = items;
+    
     return (
       <div key={id}>
-        <li>
+        <li className={oldStatus=="done"?"done-li":"pending-li"}>
           <span className="serial-number">{serialNumber}</span>
           <span className="text">{text}</span>
+          <span className="pending-section">{oldStatus}</span>
           <div className="button-section">
             <button className="btn edit">Edit</button>
-            <button className="btn pending">Pending</button>
+            <button className="btn pending"  onClick={changeLi}>{oldStatus}</button>
             <button className="btn delete">delete</button>
           </div>
         </li>
